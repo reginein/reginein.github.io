078af8b60ff32da29b6ed26d274b4034067f32c1
Regine <regine@github.com>
2015-05-24 03:21:33 +0800

    Added Contact Us page. Changed index.html page.

--- js/home.js
+++ js/home.js
@@ -1,3 +1,29 @@
-function hover(element) {
-    // element.setAttribute('src', './images/kenneth.jpg');
-}
+$(document).ready( function() {
+	 $("#submit").click(function(){
+	 	if($("#name-input").val().trim()=="")
+	 		alert("Name cannot be empty.");
+
+	 	if($("#email-input").val().trim()=="")
+	 		alert("Email cannot be empty.");
+
+	 	if(validateEmail($("#email-input").val())==false)
+	 		alert("Invalid email!");
+
+	 	if($("#phone-input").val().trim()=="")
+	 		alert("Phone cannot be empty.");
+
+	 	if(validatePhone($("#phone-input").val())==false)
+	 		alert("Invalid phone!");
+
+	 }); 
+
+	function validateEmail(email) {
+		var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+		return re.test(email);
+	}
+
+	function validatePhone(phone) {
+		var re = /^(\d|\d |\d,|\d, |\d ,|\d , )+$/i;
+		return re.test(phone);
+	}
+});
