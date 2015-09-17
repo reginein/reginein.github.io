74f7e65339e5969a2b79923823d723f9ec9c72bc
reginein <reginein@github.com>
2015-09-17 18:13:22 +0800

    Updated chart's appearance.

--- js/app/factories/mainFactory.js
+++ js/app/factories/mainFactory.js
@@ -69,13 +69,6 @@
 	  			    highlightFill: "rgba(220,220,220,0.75)",
 	  			    highlightStroke: "rgba(220,220,220,1)",
 	  			    data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-	  			},
-	  			{
-	  			    fillColor: "rgba(151,187,205,0.5)",
-	  			    strokeColor: "rgba(151,187,205,0.8)",
-	  			    highlightFill: "rgba(151,187,205,0.75)",
-	  			    highlightStroke: "rgba(151,187,205,1)",
-	  			    data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
 	  			}
             ]
         };
