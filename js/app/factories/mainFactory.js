e5dc100f980dbe6ba85341b19ddfcdad555cd87d
reginein <reginein@github.com>
2015-09-18 16:22:31 +0800

    Finalized first image for About Me carousel.

--- js/app/factories/mainFactory.js
+++ js/app/factories/mainFactory.js
@@ -2,7 +2,7 @@
     [function () {
         var aboutMeSlides = [
             {
-                image: "images/aboutme-1.jpg",
+                image: "images/aboutme-1.png",
                 active: true,
                 alt: "Regine In"
             },
