0dfa85e0fafd1e1e6e9848500a2c60c678a2a612
reginein <reginein@github.com>
2015-09-21 17:45:23 +0800

    Updated to angular1.4.6.

--- js/app/controllers/mainCtrl.js
+++ js/app/controllers/mainCtrl.js
@@ -8,16 +8,18 @@ angular.module('app').controller("MainController",
         vm.workExperiences = MainFactory.getWorkExperiences();
         vm.skills = MainFactory.getSkills();
         vm.otherSkills = MainFactory.getOtherSkills();
-        var ctx = document.getElementById("canvas").getContext("2d");
-        var chart = new Chart(ctx).HorizontalBar(vm.skills, {
-            responsive: true,
-            barShowStroke: false,
-            scaleOverride: true,
-            scaleSteps: 10,
-            scaleStepWidth: 1,
-            scaleStartValue: 0,
-            scaleFontColor: "black"
-        });
+        if (document.getElementById("canvas")) {
+            var ctx = document.getElementById("canvas").getContext("2d");
+            var chart = new Chart(ctx).HorizontalBar(vm.skills, {
+                responsive: true,
+                barShowStroke: false,
+                scaleOverride: true,
+                scaleSteps: 10,
+                scaleStepWidth: 1,
+                scaleStartValue: 0,
+                scaleFontColor: "black"
+            });
+        }
         vm.socialAccountsCaption = MainFactory.getSocialAccountsCaption();
         vm.education = MainFactory.getEducation();
 }]);
