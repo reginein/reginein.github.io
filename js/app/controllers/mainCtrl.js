eab67f378eb1386f0cd0005783bc5dd90f4a26f4
reginein <reginein@github.com>
2015-09-23 13:54:59 +0800

    Added onepage-scroll.

--- js/app/controllers/mainCtrl.js
+++ js/app/controllers/mainCtrl.js
@@ -22,7 +22,7 @@ angular.module('app').controller("MainController",
                     scaleStepWidth: 1,
                     scaleStartValue: 0,
                     scaleFontColor: "black",
-                    scaleFontSize: 14,
+                    //scaleFontSize: 14,
                 });
             }
 
@@ -38,6 +38,23 @@ angular.module('app').controller("MainController",
                 afterZoom: function (el, zoomType) { },    // This is the callback that will let you execute any function after the animation. The default value is null.
                 offsetTop: 0,                            // This allows you to define the top offset before the animation is initiated. The default value 0 so the animation will initiate right when 1 pixel of the element appears from the top of the viewport.
                 offsetBottom: 200,                       // This allows you to define the bottom offset before the animation is initiated. The default value 200 so the animation will initiate only when at least 200 pixels of the element are visible from the bottom of the viewport.
+            });
+
+            $(".main").onepage_scroll({
+                sectionContainer: ".content-section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+                easing: "linear",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+                // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+                animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+                pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+                updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+                beforeMove: function (index) { },  // This option accepts a callback function. The function will be called before the page moves.
+                afterMove: function (index) { },   // This option accepts a callback function. The function will be called after the page moves.
+                loop: true,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+                keyboard: true,                  // You can activate the keyboard controls
+                responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+                // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+                // the browser's width is less than 600, the fallback will kick in.
+                direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
             });
         });
 }]);
